d170714668425944d766c39a7b1056f95aac7538
duraid77 <82982959+duraid77@users.noreply.github.com>
2021-05-29 17:05:44 -0400

    Did small sub package for static practice

---
+++ day48_constructors_static/static_example/CalculatorTest.java
@@ -0,0 +1,10 @@
+package day48_constructors_static.static_example;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator.add(10,6);
+        Calculator cal = new Calculator();
+        cal.multiply(5,7);
+        cal.add(3,8);
+    }
+}
