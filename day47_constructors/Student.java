bafa3363cb3bdf9d4392c271a6dc05a14e82e9da
duraid77 <82982959+duraid77@users.noreply.github.com>
2021-05-29 10:18:10 -0400

    Created student object class

--- day47_constructors/Student.java
+++ day47_constructors/Student.java
@@ -1,4 +1,17 @@
 package day47_constructors;
 
 public class Student {
+    // no_args constructor
+    public Student(){
+        System.out.println("No-Args Constructor initialized");
+    }
+
+
+    public Student(String name) {
+        System.out.println("Student name: " + name);
+    }
+
+    public Student(String name, int age) {
+        System.out.println("Student " + name + " is " + age);
+    }
 }
