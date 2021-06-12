28563bb59833bce8df9c7d3956a7796dc2a59076
duraid77 <82982959+duraid77@users.noreply.github.com>
2021-06-12 15:36:23 -0400

    a lot of modifications has been done to the class.

--- day52_inheritance/AppObject.java
+++ day52_inheritance/AppObject.java
@@ -3,12 +3,21 @@ package day52_inheritance;
 public class AppObject {
     public static void main(String[] args) {
         MobileApp mobileApp = new MobileApp();
-        mobileApp.name = "youtube";
+        mobileApp.setName("youtube");
         mobileApp.useTheApp(20);
 
         Instagram instagram = new Instagram();
-        instagram.name = "Instagram";
+        instagram.setName("Instagram");
         instagram.useTheApp(30);
+        instagram.setVersion(5.3);
+        instagram.download();
+
+        Discord b22discord = new Discord();
+        b22discord.setName("Discord");
+        b22discord.setVersion(3.365);
+        b22discord.useTheApp(20);
+        b22discord.download();
+
 
     }
 
--- day52_inheritance/Discord.java
+++ day52_inheritance/Discord.java
@@ -4,9 +4,15 @@ public class Discord extends MobileApp{
 
     @Override
     public void useTheApp(int minutes){
-        System.out.println("Using Discord for " + minutes);
+        System.out.println("Using Discord for " + minutes + "for chatting");
+        super.useTheApp(35);
     }
     public void chat(String someone){
         System.out.println("Chatting with " + someone);
+    }
+
+    public void printInfo(){
+        System.out.println("App name = " + getName());
+        System.out.println("Version = " + getVersion());
     }
 }
