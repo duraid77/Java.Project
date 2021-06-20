ee3f820235e2e2a95a17d0672e2010294b4c8d34
duraid77 <82982959+duraid77@users.noreply.github.com>
2021-06-20 11:53:33 -0400

    Day 55 package and excercise has been created

---
+++ day55_abstraction_Exercise/Running.java
@@ -0,0 +1,15 @@
+package day55_abstraction_Exercise;
+
+public class Running extends Excercise{
+
+
+    @Override
+    public void perform() {
+
+    }
+
+    @Override
+    public int getCaloriesCount(int minutes) {
+        return 0;
+    }
+}
