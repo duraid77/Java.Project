f7a86f91fb0a11a43823f1f358abd564b0b7d8f0
duraid77 <82982959+duraid77@users.noreply.github.com>
2021-06-20 15:48:32 -0400

    Did a lot of flags

--- day55_abstraction_Exercise/flags/FlagDrawing.java
+++ day55_abstraction_Exercise/flags/FlagDrawing.java
@@ -1,4 +1,31 @@
 package day55_abstraction_Exercise.flags;
 
+import java.awt.*;
+
 public class FlagDrawing {
+    public static void main(String[] args) {
+        Grid grid = new Grid();
+
+        grid.setColor(0,0, Color.RED);
+
+        for (int i = 0; i < grid.getHt(); i++) {
+            grid.setColor(i,0 , Color.BLUE);
+
+        }
+        RusianFlag rusianFlag = new RusianFlag();
+        rusianFlag.draw();
+
+        FrenchFlag frenchFlag = new FrenchFlag();
+        frenchFlag.draw();
+
+
+
+        UkranianFlag ukranianFlag = new UkranianFlag();
+        ukranianFlag.draw();
+
+        IraqFlag iraqFlag = new IraqFlag();
+        iraqFlag.draw();
+
+
+    }
 }
