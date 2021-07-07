2e80619afb3a2cd98065db07b34a3d3afd382b7b
duraid77 <82982959+duraid77@users.noreply.github.com>
2021-07-06 22:07:09 -0400

    Finsihed

--- day60_exceptions/ThrowingException.java
+++ day60_exceptions/ThrowingException.java
@@ -1,4 +1,13 @@
 package day60_exceptions;
 
+import java.util.InputMismatchException;
+
 public class ThrowingException {
+    public static void main(String[] args) {
+        System.out.println("Lets create exception");
+        RuntimeException e = new RuntimeException();
+       // throw e;
+        throw new InputMismatchException();
+    }
+
 }
